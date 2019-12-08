e0d671a7e756bbf392d4ab43e59daf352588df2e
Anssi Moisio <anssi.moisio@aalto.fi>
2019-12-08 19:17:06 +0200

    it's homophone not homonym

--- Uwaterloo.py
+++ Uwaterloo.py
@@ -2,29 +2,30 @@ import process_data
 import pickle
 
 
-def get_homonym_dict():
+def get_homophone_dict():
     """
     Get a dictionary of similarly sounding words from "homonyms.txt"
     """
-    with open('homonyms.txt', 'r') as f:
+    with open('homophones.txt', 'r') as f:
         l = f.read().splitlines()
 
     l = [line.replace(',', ' ').split() for line in l]
 
-    homonym_dict = {}
+    homophone_dict = {}
     for line in l:
         for word in line:
-            if word not in homonym_dict:
-                homonym_dict[word] = set(line)
+            if word not in homophone_dict:
+                homophone_dict[word] = set(line)
             else:
-                homonym_dict[word] = homonym_dict[word].union(set(line))
+                homophone_dict[word] = homophone_dict[word].union(set(line))
     
-    for word, set_of_homonyms in homonym_dict.items():
-        if word in set_of_homonyms:
-            set_of_homonyms.remove(word)
+    for word, set_of_homophones in homophone_dict.items():
+        if word in set_of_homophones:
+            set_of_homophones.remove(word)
 
-    return homonym_dict
+    return homophone_dict
 
+a = get_homophone_dict()
 
 def select_tom_swifty(puns, results):
     """
